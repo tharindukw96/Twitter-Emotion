1ac7d10d6d530fd1c07f6e5b3476f85a7e5f1e65
tharindukw96 <33366684+tharindukw96@users.noreply.github.com>
2019-03-05 02:08:58 +0530

    Update web Theme

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
 //set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/favicon.ico', express.static('public/favicon.ico'));
+
+
 
 // set the views
 app.set('views', path.join(__dirname, 'views'));
@@ -13,12 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    //res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Credentials', true);
+    //res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    //res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-
+    
     /*
     var http = require('http');
     var str = '';
